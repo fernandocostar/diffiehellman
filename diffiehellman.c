561bb1b70397860abc8f3a15504ce02d0da153b2
fernandocostar <fernandocr@id.uff.br>
2017-10-02 13:02:33 -0300

    Comments added

--- diffiehellman.c
+++ diffiehellman.c
@@ -9,15 +9,15 @@ TRABALHO 2 - SEGURANCA DA INFORMACAO 2017.2
 */
 
 uint64_t powmod(uint64_t g, uint64_t c, uint64_t p){ //funcao que retorna g^c mod p usando inteiros de 64 bits
-
+	//usando a logica de composicao binaria para o expoente e decomposicao de mesma base para g chegamos no algoritmo abaixo
     uint64_t res = 1; //inicializa a variavel do resultado
     
     g = g % p; //g se torna o modulo dele mesmo por p
     while (c > 0){
         if (c & 1) //se o bit menos significativo for 1 entao ele sera impar
-            res = (res*g) % p;
+            res = (res*g) % p; //multiplicando o resultado pelo numero sempre utilizando modulo
  
-        // y must be even now
+        //nosso expoente terá que ser par
         c = c>>1; //usando bitshift retiramos do expoente atual uma potencia de dois tranformando-o em sua metade
         g = (g*g) % p; //fazemos modulo do quadrado do numero
     }
@@ -31,10 +31,10 @@ int main(){
 	rand2 = 846930886; //rand();
 	scanf("%"SCNu64,&p);
 	scanf("%"SCNu64,&g);
-	A = powmod(g, rand1, p);
-	B = powmod(g, rand2, p);
-	result1 = powmod(A, rand2, p);
-	result2 = powmod(B, rand1, p);
+	A = powmod(g, rand1, p); //fazemos A = g^a mod p
+	B = powmod(g, rand2, p); //fazemos B = g^b mod p
+	result1 = powmod(A, rand2, p); //geramos o valor comum com A^b mod p 
+	result2 = powmod(B, rand1, p); //geramos o valor comum com B^a mod p
 	printf("Alice:");
 	printf("%\n"SCNu64, A);
 	printf("\n");
@@ -49,10 +49,3 @@ int main(){
 	printf("\n");
 	return 0;
 }
-
-/*
-A = g^a mod p
-B = g^b mod p
-
-A^b mod p = B^a mod p
-*/
